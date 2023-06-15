6209b7a82a34c4fecb0d9143419c8e5197e2736f
ChiuKuanWei <harry20040723@gmail.com>
2023-06-15 22:13:45 +0800

    20230615

---
+++ PictureSave_Project/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace PictureSave_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_LoadPicture = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(876, 464);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_LoadPicture
+            // 
+            this.btn_LoadPicture.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_LoadPicture.Location = new System.Drawing.Point(347, 479);
+            this.btn_LoadPicture.Name = "btn_LoadPicture";
+            this.btn_LoadPicture.Size = new System.Drawing.Size(141, 43);
+            this.btn_LoadPicture.TabIndex = 1;
+            this.btn_LoadPicture.Text = "Load Picture";
+            this.btn_LoadPicture.UseVisualStyleBackColor = true;
+            this.btn_LoadPicture.Click += new System.EventHandler(this.btn_LoadPicture_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(876, 464);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(876, 545);
+            this.Controls.Add(this.btn_LoadPicture);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "圖片儲存系統";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_LoadPicture;
+    }
+}
+
---
+++ PictureSave_Project/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureSave_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 初始化右鍵選單和選單項目
+            InitializeContextMenu();
+        }
+
+        private void btn_LoadPicture_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedImage = openFileDialog.FileName;
+                pictureBox1.Image = new System.Drawing.Bitmap(selectedImage);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void InitializeContextMenu()
+        {
+            ContextMenuStrip contextMenuStrip = new ContextMenuStrip();
+            ToolStripMenuItem saveImageMenuItem = new ToolStripMenuItem();
+            saveImageMenuItem.Text = "另存圖檔";
+            saveImageMenuItem.Click += SaveImageMenuItem_Click;
+
+            contextMenuStrip.Items.Add(saveImageMenuItem);
+
+            pictureBox1.ContextMenuStrip = contextMenuStrip;
+        }
+
+        private void SaveImageMenuItem_Click(object sender, EventArgs e)
+        {
+            if(pictureBox1.Image == null)
+            {
+                MessageBox.Show("請先載入圖片!!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "JPEG Image|*.jpg|PNG Image|*.png";
+                saveFileDialog.Title = "另存圖檔";
+                //saveFileDialog.FileName = "image";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Image image = pictureBox1.Image;
+                    if (image != null)
+                    {
+                        try
+                        {
+                            image.Save(saveFileDialog.FileName);
+                            MessageBox.Show("儲存成功!!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("儲存失敗:" + ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            GC.Collect();
+        }
+    }
+}
